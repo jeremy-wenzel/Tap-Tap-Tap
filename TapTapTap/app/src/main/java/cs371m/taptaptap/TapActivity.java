30a0e463a64d136573e137dce764107f3ef2642a
Sassafrass6 <Frank@Franks-MacBook-Pro.local>
2016-03-28 19:38:05 -0500

    First Prototype

---
+++ TapTapTap/app/src/main/java/cs371m/taptaptap/TapActivity.java
@@ -0,0 +1,13 @@
+package cs371m.taptaptap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap);
+    }
+}
