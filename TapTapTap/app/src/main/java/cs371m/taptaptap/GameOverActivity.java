b13e093b33f2cdc99f0ad917fd716e628e8fbfe4
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2016-04-03 16:57:36 -0500

    Created skeleton for activities

---
+++ TapTapTap/app/src/main/java/cs371m/taptaptap/GameOverActivity.java
@@ -0,0 +1,13 @@
+package cs371m.taptaptap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+    }
+}
