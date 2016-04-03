53376f55ba24d53a01715b3498e35390efdb9ea9
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2016-04-03 17:17:18 -0500

    Connected MainLandingActivity to other activities

--- TapTapTap/app/src/main/java/cs371m/taptaptap/MainLandingActivity.java
+++ TapTapTap/app/src/main/java/cs371m/taptaptap/MainLandingActivity.java
@@ -1,7 +1,9 @@
 package cs371m.taptaptap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainLandingActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainLandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_landing);
+    }
+
+    public void newGame(View view) {
+        Intent intent = new Intent(this, GameTypeActivity.class);
+        startActivity(intent);
+    }
+
+    public void highScores(View view) {
+        Intent intent = new Intent(this, UnderConstruction.class);
+        startActivity(intent);
+    }
+
+    public void settings(View view) {
+        Intent intent = new Intent(this, UnderConstruction.class);
+        startActivity(intent);
     }
 }
