2a64269e51efe9d50074e15645f8df1244f96a4c
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2016-04-21 22:09:35 -0500

    Cleanup

--- TapTapTap/app/src/main/java/cs371m/taptaptap/MainLandingActivity.java
+++ TapTapTap/app/src/main/java/cs371m/taptaptap/MainLandingActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
